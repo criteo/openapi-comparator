f3d3b3b85665dacacab9dba739e51b187d734a48
paul-mathon <p.mathon@criteo.com>
2023-01-06 17:12:18 +0100

    💡 Add missing comments for public API
    This should solve the warnings at compilation time

--- src/Criteo.OpenApi.Comparator/MessageType.cs
+++ src/Criteo.OpenApi.Comparator/MessageType.cs
@@ -5,8 +5,19 @@
     /// </summary>
     public enum MessageType
     {
+        /// <summary>
+        /// If an OpenAPI element is added in the new version
+        /// </summary>
         Addition,
+        
+        /// <summary>
+        /// If an OpenAPI element is updated in the new version
+        /// </summary>
         Update,
+        
+        /// <summary>
+        /// If an OpenAPI element is removed in the new version
+        /// </summary>
         Removal,
     }
 }
