a12097c14af27a9236b1f0cfe3bbaab0a11658b9
t.nion <t.nion@criteo.com>
2023-05-16 19:53:39 +0200

    Allow directly comparing files from their URLs.
    This answers request: https://github.com/criteo/openapi-comparator/issues/24

--- src/Criteo.OpenApi.Comparator.Cli/Program.cs
+++ src/Criteo.OpenApi.Comparator.Cli/Program.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text.Json;
 
 namespace Criteo.OpenApi.Comparator.Cli
@@ -25,7 +26,7 @@ namespace Criteo.OpenApi.Comparator.Cli
             {
                 return 1;
             }
-            
+
             var options = parserResult.Value;
 
             var oldFileFound = TryReadFile(options.OldSpec, out var oldOpenApiSpecification);
@@ -36,15 +37,37 @@ namespace Criteo.OpenApi.Comparator.Cli
                 Console.WriteLine("Exiting.");
                 return 1;
             }
-            
+
             var differences = OpenApiComparator.Compare(oldOpenApiSpecification, newOpenApiSpecification);
 
             DisplayOutput(differences, options.OutputFormat);
-            
+
             return 0;
         }
 
         private static bool TryReadFile(string path, out string fileContent)
+        {
+            try
+            {
+                var uri = new Uri(path);
+                if (uri.IsFile)
+                {
+                    return TryReadLocalFile(path, out fileContent);
+                }
+                else
+                {
+                    return TryReadRemoteFile(path, out fileContent);
+                }
+            }
+            catch (UriFormatException)
+            {
+                Console.WriteLine($"Failed to interpret the provided URI: {path}.");
+                fileContent = null;
+            }
+            return false;
+        }
+
+        private static bool TryReadLocalFile(string path, out string fileContent)
         {
             try
             {
@@ -57,6 +80,33 @@ namespace Criteo.OpenApi.Comparator.Cli
                 fileContent = null;
                 return false;
             }
+        }
+
+        private static bool TryReadRemoteFile(string path, out string fileContent)
+        {
+            try
+            {
+                using var client = new HttpClient();
+                fileContent = client.GetStringAsync(path).Result;
+                return true;
+            }
+            catch (HttpRequestException exception)
+            {
+                Console.WriteLine($"Http request failed on {path}: {exception.Message}");
+            }
+            catch (AggregateException exception)
+            {
+                var stringWriter = new StringWriter();
+                stringWriter.WriteLine($"Exception caught while waiting for the Http request result from {path}:");
+                exception.Handle((innerException) =>
+                {
+                    stringWriter.Write($"- {innerException.GetType()}: {exception.Message}");
+                    return true;
+                });
+                Console.WriteLine(stringWriter.ToString());
+            }
+            fileContent = null;
+            return false;
         }
 
         private static void DisplayOutput(IEnumerable<ComparisonMessage> differences, OutputFormat outputFormat)
