ad4bedf254a33f97663ac61d7e32b3cbbf2077a2
Will Westrop <wwestrop@users.noreply.github.com>
2025-02-18 17:40:35 +0000

    Improve reporting of altered enum values

--- src/Criteo.OpenApi.Comparator/Comparators/Extensions/OpenApiSchemaExtensions.cs
+++ src/Criteo.OpenApi.Comparator/Comparators/Extensions/OpenApiSchemaExtensions.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Criteo Technology. All rights reserved.
 // Licensed under the Apache 2.0 License. See LICENSE in the project root for license information.
 
+using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 
 namespace Criteo.OpenApi.Comparator.Comparators.Extensions
@@ -9,5 +10,12 @@ namespace Criteo.OpenApi.Comparator.Comparators.Extensions
     {
         internal static bool IsPropertyRequired(this OpenApiSchema schema, string propertyName) =>
             schema.Required != null && schema.Required.Contains(propertyName);
+
+        internal static string StringValue(this IOpenApiAny openApiAny) =>
+            openApiAny switch
+            {
+                OpenApiString openApiString => openApiString.Value,
+                _ => openApiAny.ToString(),
+            };
     }
 }
